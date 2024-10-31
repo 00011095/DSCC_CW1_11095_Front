3552dbe1b5cab8a8e6bc0cbf82fa850d0010a861
00011095 <wiut00011095@gmail.com>
2024-10-31 17:21:53 +0500

    Controllers added

---
+++ DSCC_CW1_11095_Front/Controllers/ProducerController.cs
@@ -0,0 +1,171 @@
+﻿using DSCC_CW1_11095_Front.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DSCC_CW1_11095_Front.Controllers
+{
+    public class ProducerController : Controller
+    {
+        private readonly string BaseUrl = "http://ec2-16-170-251-213.eu-north-1.compute.amazonaws.com/"; 
+
+        // GET: ProducerController
+        public async Task<ActionResult> Index()
+        {
+            List<ProducerDto> producers = new List<ProducerDto>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/producer/GetAllProducers");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    producers = JsonConvert.DeserializeObject<List<ProducerDto>>(responseContent);
+                }
+            }
+            return View(producers);
+        }
+
+        // GET: ProducerController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            ProducerDto producer = new ProducerDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/producer/GetProducerById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    producer = JsonConvert.DeserializeObject<ProducerDto>(responseContent);
+                }
+            }
+            return View(producer);
+        }
+
+        // GET: ProducerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProducerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProducerCreateDto producer)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(producer);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("api/producer/CreateProducer", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(producer);
+        }
+
+        // GET: ProducerController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            ProducerDto producer = new ProducerDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/producer/GetProducerById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    producer = JsonConvert.DeserializeObject<ProducerDto>(responseContent);
+                }
+            }
+            return View(producer);
+        }
+
+        // POST: ProducerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, ProducerCreateDto producerUpdateDto)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(producerUpdateDto);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PutAsync($"api/producer/UpdateProducer/{id}", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(producerUpdateDto);
+        }
+
+        // GET: ProducerController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            ProducerDto producer = new ProducerDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/producer/GetProducerById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    producer = JsonConvert.DeserializeObject<ProducerDto>(responseContent);
+                }
+            }
+            return View(producer);
+        }
+
+        // POST: ProducerController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.DeleteAsync($"api/producer/DeleteProducer/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ DSCC_CW1_11095_Front/Controllers/ProductController.cs
@@ -0,0 +1,171 @@
+﻿using DSCC_CW1_11095_Front.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DSCC_CW1_11095_Front.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly string BaseUrl = "http://ec2-16-170-251-213.eu-north-1.compute.amazonaws.com/"; 
+
+        // GET: ProductController
+        public async Task<ActionResult> Index()
+        {
+            List<ProductDto> products = new List<ProductDto>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/product/GetAllProducts");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    products = JsonConvert.DeserializeObject<List<ProductDto>>(responseContent);
+                }
+            }
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            ProductDto product = new ProductDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/product/GetProductById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    product = JsonConvert.DeserializeObject<ProductDto>(responseContent);
+                }
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProductCreateDto product)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(product);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("api/product/CreateProduct", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            ProductDto product = new ProductDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/product/GetProductById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    product = JsonConvert.DeserializeObject<ProductDto>(responseContent);
+                }
+            }
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, ProductCreateDto productUpdateDto)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(productUpdateDto);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PutAsync($"api/product/UpdateProduct/{id}", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(productUpdateDto);
+        }
+
+        // GET: ProductController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            ProductDto product = new ProductDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/product/GetProductById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    product = JsonConvert.DeserializeObject<ProductDto>(responseContent);
+                }
+            }
+            return View(product);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.DeleteAsync($"api/product/DeleteProduct/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
+        }
+    }
+}
